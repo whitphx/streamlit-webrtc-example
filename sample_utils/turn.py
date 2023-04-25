5bebc6e78edcf6ee8f1b809848e7d26c8bf4ee6d
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-04-25 15:35:11 +0200

    Fix the object detection demo to use Twilio TURN server

--- sample_utils/turn.py
+++ sample_utils/turn.py
@@ -1,26 +1,25 @@
 import logging
 import os
 
-import requests
+from twilio.rest import Client
 import streamlit as st
 
 logger = logging.getLogger(__name__)
 
 
-OPEN_RELAY_API_HOST = os.environ.get("OPEN_RELAY_API_HOST")
-OPEN_RELAY_API_KEY = os.environ.get("OPEN_RELAY_API_KEY")
-
-
 @st.cache_data
 def get_ice_servers():
-    if not OPEN_RELAY_API_HOST or not OPEN_RELAY_API_KEY:
+    try:
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    except KeyError:
         logger.warning(
-            "Open Relay API host or key is not set. Fallback to a free STUN server from Google."  # noqa: E501
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
         )
         return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
-    # Get response from Open Relay API
-    response = requests.get(
-        f"https://{OPEN_RELAY_API_HOST}/api/v1/turn/credentials?apiKey={OPEN_RELAY_API_KEY}"  # noqa: E501
-    )
-    return response.json()
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
